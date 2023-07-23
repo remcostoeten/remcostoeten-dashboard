0b342c3e56549c10f111b4d5707a0b9fe994cfd2
remcostoeten <remcostoeten@hotmail.com>
2023-07-23 03:28:22 +0200

    wip

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+
 import { marketingConfig } from "@/config/marketing"
+import { CursorProvider } from "@/lib/CursorContext"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
-import { CursorProvider } from "@/lib/CursorContext"
 
 interface MarketingLayoutProps {
   children: React.ReactNode
@@ -14,26 +15,28 @@ export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <header className="container z-40 ">
-        <div className="flex h-20 items-center justify-between py-6">
-          <MainNav items={marketingConfig.mainNav} />
-          <nav>
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: "secondary", size: "sm" }),
-                "px-4"
-              )}
-              showhand="true"
-            >
-              Login
-            </Link>
-          </nav>
-        </div>
-      </header>
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
+    <div className="flex min-h-screen flex-col page-wrapper">
+      <div className="page-wrapper__inner">
+        <header className="container z-40 ">
+          <div className="flex h-20 items-center justify-between py-6">
+            <MainNav items={marketingConfig.mainNav} />
+            <nav>
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4"
+                )}
+                showhand="true"
+              >
+                Login
+              </Link>
+            </nav>
+          </div>
+        </header>
+        <main className="flex-1">{children}</main>
+        <SiteFooter />
+      </div>
     </div>
   )
 }
